1b96dab33e636b068df146c9387554e6a2405bcb
justinmklam <justinmklam@gmail.com>
2019-08-16 11:37:09 -0700

    Remove camera emulator

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from importlib import import_module
 import os
 from flask import Flask, render_template, Response
 
-# import camera driver
+# import camera driver. Otherwise use pi camera by default
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
-    from camera import Camera
+    from camera_pi import Camera
 
-# Raspberry Pi camera module (requires picamera package)
-# from camera_pi import Camera
 
 app = Flask(__name__)
 
